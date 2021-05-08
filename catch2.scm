544e48da0b525c56e667f1c34181c1e8d8891d99
hdevarajan <hdevarajan@penguin>
2021-05-08 14:40:13 -0500

    fixed catch2 syntax

--- catch2.scm
+++ catch2.scm
@@ -1,5 +1,6 @@
 (define-module  (catch2)
   #:use-module (guix)
+  #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system cmake)
   #:use-module (guix licenses))
@@ -15,8 +16,8 @@
 	   (base32 "0xx8jwb53477j0h7j15dz3q47i8ih89wgrwqa3s79dbyiw05pp7"))))
  (build-system cmake-build-system)
  (arguments
-     `(#:configure-flags
-       (list "-DBUILD_TESTING=OFF")))
+  `(#:configure-flags '("-DBUILD_TESTING=OFF"))
+ )
  (synopsis "Catch2 Testing framework for C++")
  (description "Catch2 is mainly a unit testing framework for C++, but it also provides basic micro-benchmarking features, and simple BDD macros.")
  (home-page "https://github.com/catchorg/Catch2/")
